bbd3685a22070af4a39e42402c28ada5058d5600
Melody Ferraro <melodyferraro@hotmail.com>
2022-03-24 20:13:24 -0300

    puerto para Heroku

--- app.js
+++ app.js
@@ -3,31 +3,31 @@ const app = express();
 const path = require("path");
 
 app.use(express.static("public"));
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Servidor corriendo");
 });
-app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname,'./views/home.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/home.html"));
 });
 
-app.get('/ayuda', (req, res)=>{
-    res.sendFile(path.join(__dirname,'./views/ayuda.html'));
+app.get("/ayuda", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/ayuda.html"));
 });
-app.get('/ofertas', (req, res)=>{
-    res.sendFile(path.join(__dirname,'./views/ofertas.html'));
+app.get("/ofertas", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/ofertas.html"));
 });
-app.get('/tiendasoficiales', (req, res)=>{
-    res.sendFile(path.join(__dirname,'./views/tiendasoficiales.html'));
+app.get("/tiendasoficiales", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/tiendasoficiales.html"));
 });
-app.get('/vender', (req, res)=>{
-    res.sendFile(path.join(__dirname,'./views/vender.html'));
+app.get("/vender", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/vender.html"));
 });
-app.get('/register', (req, res)=>{
-    res.sendFile(path.join(__dirname,'./views/register.html'));
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/register.html"));
 });
-app.get('/miscompras', (req, res)=>{
-    res.sendFile(path.join(__dirname,'./views/miscompras.html'));
+app.get("/miscompras", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/miscompras.html"));
 });
-app.get('/login', (req, res)=>{
-    res.sendFile(path.join(__dirname,'./views/login.html'));
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/login.html"));
 });
